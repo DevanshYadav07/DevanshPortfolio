ed62e4522937170f6c9ac5b09f9dc185a0cad68b
DevanshYadav0702 <devanshyadav0702@gmail.com>
2024-07-30 00:13:12 +0530

    removed images added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
-
+import Favicon from '@/app/favicon.ico'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Devansh Portfolio",
   description: "Personal Portfolio to describe ",
+  icons:
+  {
+    icon:Favicon.src
+  }
 };
 
 export default function RootLayout({
@@ -17,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head >
+        <link rel="icon" href={Favicon.src} />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
